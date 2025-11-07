1be9bf12b7ba0316cba54ca72f96965c6979bd41
Erick-XMB <erick.martinezkmn@ciencias.unam.mx>
2025-11-06 20:07:09 -0600

    comentarios

--- src/Practica03.hs
+++ src/Practica03.hs
@@ -45,18 +45,12 @@ isIn x (y:ys) = if x == y then True else isIn x ys
 
 
 -- Funcion auxiliar para que no se repitan elementos en el ejercicio 1
-noRepetir :: Eq a => [a] -> [a] -- Recibe una lista de elementos "a" y devuelve una lista de lo mismo
-                                -- Eq es para que sean comparables
+noRepetir :: Eq a => [a] -> [a] 
 noRepetir = noRepAux [] -- noRepetir llama la funcion auxiliar noRepAux y [] es una lista vacia
   where
-    noRepAux _ [] = [] -- Si hay una lista vacia devolvemos la lista vacia
+    noRepAux _ [] = [] 
     noRepAux yaVistos (x:xs) = if (isIn x yaVistos) then (noRepAux yaVistos xs) else (x : noRepAux (yaVistos ++ [x]) xs)
-    -- Usamos isIn pues asi podemos ver si un elemento ya esta en una lista
-    -- aqui se ve que:
-    -- en el if vemos si "x" ya ha sido visto antes
-    -- Si esta en yaVistos, no se agrega a la lista resultante, y se sigue el proceso con el resto "xs"
-    -- Si no esta en yaVistos, lo agregamos al resultado con ":" y se sigue con "xs"
-    -- tambien se agrega x a la lista de yaVistos, asi no se puede volver a repetir
+-- aqui recorremos la lista, y si el elemento x esta en yaVistos se quita, sino se mantiene y se sigue revisando
 
 
 -- Ejercicio 1 devuelve el conjunto formado por todas las variables proposicionales que aparecen en f.
@@ -75,7 +69,7 @@ variables (Syss p q) = noRepetir (variables p ++ variables q)
 interpretacion :: Prop -> Estado -> Bool
 -- i es de estados
 interpretacion (Var x) i = isIn x i -- Las variables son verdad si aparece en el estado
-interpretacion (Not p) i = not (interpretacion p i) -- usamos not para "invertir" el valor de la formula
+interpretacion (Not p) i = not (interpretacion p i) -- usamos not para "negar" el valor de la formula
 --Usamos || y && para aplicarlas a las formulas
 interpretacion (Or p q) i =  interpretacion p i || interpretacion q i
 interpretacion (And p q) i =  interpretacion p i && interpretacion q i
@@ -107,13 +101,10 @@ modelos p = [i | i <- estadosPosibles p, interpretacion p i]
 sonEquivalentes :: Prop -> Prop -> Bool
 sonEquivalentes p q = checarEstados (estadosPosibles (Syss p q))
 -- llamamos a la funcion de chacarEstados para ver que "Syss p q" es verdadera en todos los estados
-    where   
-        -- como es el caso base, si ya no hay estados que revisar entonces se va a deolver true
-        checarEstados [] = True
-        -- en este caso tomamos el primer estado de una lista de estados
-        -- "interpretacion (Syss p q) i" nos dice si es verdad en ese caso, y si es asi volvemos a checar los estados que faltan
-        -- si hay un estado donde no se cumple, entonces vamos a devolver "False"
+    where    
+        checarEstados [] = True --si ya no hay estados que revisar entonces se va a deolver true
         checarEstados (i:is) = if (interpretacion (Syss p q) i) then (checarEstados is) else (False)
+        -- checamos que "interpretacion (Syss p q) i" sea verdad, si si es se sigue revisando, y si no devolvemos false
 
 
 -- Ejercicio 6 dada una fórmula proposicional, nos dice si es una tautología.
